c038fa3086cffccee0e60e9a85f2f0df75dac475
PreethiSakar <preethisakar7@gmail.com>
2020-04-24 13:24:16 +0530

    Updates

---
+++ precache-manifest.e288a5c191ac1693247746aced21dcfa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33128f9702fd8220a05d2ad3cf30b7a",
+    "url": "/pokemon-search/index.html"
+  },
+  {
+    "revision": "2457f45dda362a35d371",
+    "url": "/pokemon-search/static/css/2.e91a0e2d.chunk.css"
+  },
+  {
+    "revision": "ea88b54034726f539993",
+    "url": "/pokemon-search/static/css/main.fcbd77c9.chunk.css"
+  },
+  {
+    "revision": "2457f45dda362a35d371",
+    "url": "/pokemon-search/static/js/2.e396cf63.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/pokemon-search/static/js/2.e396cf63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea88b54034726f539993",
+    "url": "/pokemon-search/static/js/main.017a1c11.chunk.js"
+  },
+  {
+    "revision": "d9a6d763430eb50301ba",
+    "url": "/pokemon-search/static/js/runtime-main.a6ee3edd.js"
+  },
+  {
+    "revision": "f234edf1d7cf92f8c8d8c1c8b64bde8f",
+    "url": "/pokemon-search/static/media/Pokemon_logo.f234edf1.png"
+  }
+]);
